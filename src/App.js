767ee1e9c2c21b0112eca12df88fc97f89ac9c96
Zestymcspicy <halbeckerman@yahoo.com>
2020-07-12 14:06:01 -0500

    css for footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
-// import ReactDom from 'react-dom';
-// import './styles.css';
-import Box from './box';
-// import useDebounce from './use-debounce';
-import CharacterCrosses from './characterCrosses'
+// import Box from './box';
+// import CharacterCrosses from './characterCrosses'
 import MainBanner from './mainBanner'
 import Home from './home'
 import Footer from './footer'
