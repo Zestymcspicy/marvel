57bf5971b799e95eaabcf85f65b8e37961e7518c
Zestymcspicy <halbeckerman@yahoo.com>
2020-07-18 19:10:13 -0500

    dates are rendering on comics

--- src/issue.js
+++ src/issue.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export default function Issue(props){
+  let date = new Date(props.comic.dates[0].date)
+  date = date.toDateString()
   let imagePath = `${props.comic.images[0].path}.${props.comic.images[0].extension}`
   return(
     <div className="issue-box">
     <div className="issue">
     <img src={imagePath} alt={props.comic.title}/>
     <span className="comic-title">{props.comic.title}</span>
+    <span className="comic-date">{date}</span>
     </div>
     </div>
   )
