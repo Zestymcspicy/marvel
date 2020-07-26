ad9fe91c90e1bc05be82e85d7ce60622845069ef
Zestymcspicy <halbeckerman@yahoo.com>
2020-07-26 16:36:19 -0500

    switching icons to svgs

---
+++ src/browseIcon.js
@@ -0,0 +1,95 @@
+import React from 'react'
+
+export default function BrowseIcon() {
+  return(
+  <svg
+     xmlns:dc="http://purl.org/dc/elements/1.1/"
+     xmlns:cc="http://creativecommons.org/ns#"
+     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+     xmlns:svg="http://www.w3.org/2000/svg"
+     xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink"
+     xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+     xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+     version="1.1"
+     id="svg4747"
+     width="42"
+     height="42"
+     viewBox="0 0 42 42"
+     sodipodi:docname="browse.svg"
+     inkscape:version="0.92.4 (5da689c313, 2019-01-14)">
+    <metadata
+       id="metadata4753">
+      <rdf:RDF>
+        <cc:Work
+           rdf:about="">
+          <dc:format>image/svg+xml</dc:format>
+          <dc:type
+             rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        </cc:Work>
+      </rdf:RDF>
+    </metadata>
+    <defs
+       id="defs4751" />
+    <sodipodi:namedview
+       pagecolor="#ffffff"
+       bordercolor="#666666"
+       borderopacity="1"
+       objecttolerance="10"
+       gridtolerance="10"
+       guidetolerance="10"
+       inkscape:pageopacity="0"
+       inkscape:pageshadow="2"
+       inkscape:window-width="1730"
+       inkscape:window-height="991"
+       id="namedview4749"
+       showgrid="false"
+       inkscape:zoom="19.238095"
+       inkscape:cx="21"
+       inkscape:cy="21"
+       inkscape:window-x="95"
+       inkscape:window-y="0"
+       inkscape:window-maximized="0"
+       inkscape:current-layer="svg4747" />
+    <image
+       width="42"
+       height="42"
+       preserveAspectRatio="none"
+       style="image-rendering:optimizeQuality"
+       xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACoAAAAqCAIAAABKoV4MAAAAAXNSR0IArs4c6QAAAARnQU1BAACx
+  jwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAANdSURBVFhH7ZZJTypBFIUbEAVHFEQhDMFpoQE3
+  /P+1OzE4hAQJBImCcUAZBKf3JVXp8KTorkZ5LHxnAUVVN+cO595brrW1NWN6cMvvKeE//RTxu+kd
+  FJ7X652dnfV4PG63+/39/e3trdfrfXx8yOOxoEUPcTwej0QiKysrWDAzMwN3v99/enq6vr6uVqus
+  5aMOYUMPE6wHBwdLS0tyyzDwmADIH4bx8vJyenpaLpc/Pz/lljas6H0+387OztbW1tzcHBzNZvPq
+  6gqPX19ficfy8nI0Gl1fX8dEDCoWi2dnZ0RFvqwHK/pMJrO7u4ujt7e35+fnfH7xz+VyBYPB/f39
+  jY0Nji4vL09OThypweP3++VyAPxvKpWCnv8itUdHR61WS579jU6nwwMIAjtWV1cJzN3dnTzTgLrw
+  FhcXiTmLm5sb8orOxb4SmIjTjUYDo1EJpsgDDajpSSpJoa4uLi7wT+6OBhYcHx93u110sLe3hx3y
+  wA4Kev4iFouxqNVqyE1s2gJJIkwWVIp+ABT0FLcQBJF3pCPoEaD5ug4U9NhOXbEgnWJHE6QJEPn5
+  +Xm5ZQcFPdx0VjQM5JYeUKh45Vvei4AP9jVNmK/otz8FB50LPwiAoxIC0ItXKBmxYwsFfbvdFjEM
+  h8NiRxN0C8LO+BnVo4ahoH9+fsYCFolEwkEFu91MRZ5/fHz8lveAPsonnZxWKnZsQcelWbGgXphP
+  YtMWavpKpfLw8ED/yWazOjJmJB4eHvJJ2Bg8clcD6pGD+OliyWSSmRsKhbhTWExS+gzDaXNzk2fy
+  +byjkaOmBzQQ6gfuhYUFoooY0cRwRaEP/DZFStbplWKtA6t5j44Yu+l0WpQTBjEFUBZekhfuP5jF
+  pUM8bKJUKjEANe8d9ne9QCCAf2iQTiB2SA2WAdaUGSoBDDqz7ZB+BqBO8xkZfBPImFlCRpmnoh+L
+  2xXFTTCYyIVCgYDjLpkSFmAxotHJgr33w4BDOQm3t7fRIMaJnzpZsPd+GKOiSgoIFfNe5IVOgGis
+  YzAOvQWEMPWz8MP04P7+nirlAo4FhIHkYgG3ZGW+fp4eKLNA7xKng5gIPfiSBe4/9Xp9eBRNih6Y
+  WSASuVyO+MuDAYxTeI7A4CDrdAhl7idObw31wP1n+M30hvEHOSjYIEw+EL4AAAAASUVORK5CYII=
+  "
+       id="image4755"
+       x="0"
+       y="0" />
+    <g
+       id="g8500">
+      <path
+         style="fill:#494949;fill-opacity:1"
+         d="M 28.074269,32.575674 C 24.183165,28.610929 23.461457,28.254255 21.126467,29.142017 14.403617,31.69804 7,26.290034 7,18.823275 7,15.581119 7.6601103,14.13989 10.4,11.4 c 4.730425,-4.7304251 9.469575,-4.7304251 14.2,0 3.643632,3.643632 4.537491,8.606061 2.302423,12.782325 -0.949872,1.774851 -0.475589,2.682326 3.524306,6.743286 C 34.441357,35.001527 35.30163,37 33.041541,37 32.697722,37 30.46245,35.009053 28.074269,32.575674 Z m -5.150994,-7.164568 c 7.067733,-5.947105 0.858756,-17.6984145 -7.884932,-14.923276 -6.3965059,2.030173 -8.0290102,9.675349 -3.115266,14.589093 3.62524,3.625241 6.9683,3.726802 11.000198,0.334183 z"
+         id="path8504"
+         inkscape:connector-curvature="0" />
+      <path
+         style="fill:#333333;fill-opacity:1"
+         d="M 27.651949,32.970271 C 24.116977,29.330117 23.487353,29.05834 19.913249,29.629863 12.137815,30.873205 7,26.489553 7,18.612106 7,9.6877696 16.711363,4.8100328 24.060525,10.043098 c 4.244289,3.0222 6.023446,12.550903 2.724173,14.589966 -0.537074,0.33193 1.144827,2.75722 3.737559,5.389532 4.330589,4.396702 5.152398,7.027964 2.168726,6.943814 -0.655041,-0.01848 -2.922606,-1.816737 -5.039034,-3.996139 z M 23.75,24.406587 C 26.661353,21.148312 26.614254,16.82564 23.629592,13.355764 16.873749,5.5016282 4.6531192,15.348946 10.606999,23.849299 13.91892,28.577729 19.800154,28.827105 23.75,24.406587 Z"
+         id="path8502"
+         inkscape:connector-curvature="0" />
+    </g>
+  </svg>
+)
+}
